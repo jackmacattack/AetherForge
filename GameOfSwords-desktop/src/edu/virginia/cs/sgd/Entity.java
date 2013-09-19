9f9d996354cb547ecfad1c89a11588993651411b
Mitchell Smith <zephyrthenoble@gmail.com>
2013-09-18 22:00:03 -0400

    Added some more stuff

--- GameOfSwords-desktop/src/edu/virginia/cs/sgd/Entity.java
+++ GameOfSwords-desktop/src/edu/virginia/cs/sgd/Entity.java
@@ -1,12 +1,39 @@
 package edu.virginia.cs.sgd;
 
+/**
+ * @author Mitchell
+ *
+ */
+
 public class Entity {
 
-	
+	//the x position of an Entity
 	int xpos;
+	//the y position of an Entity
 	int ypos;
 	
 	
+	//probably not accessed, just set to some standard width and height of the grid
+	int width;
+	int height;
+	
+	//the image that is displayed
+	//sprite Sprite
+	
+	public int getXpos() {
+		return xpos;
+	}
+	public void setXpos(int xpos) {
+		this.xpos = xpos;
+	}
+	public int getYpos() {
+		return ypos;
+	}
+	public void setYpos(int ypos) {
+		this.ypos = ypos;
+	}
+
+
 	public Entity()
 	{
 		

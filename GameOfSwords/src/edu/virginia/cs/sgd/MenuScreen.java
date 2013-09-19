1df3d5c56fc35d3a512a4674ea4a6c49cd29dfef
James <jqw3ha@virginia.edu>
2013-09-18 23:20:46 -0400

    Screens

---
+++ GameOfSwords/src/edu/virginia/cs/sgd/MenuScreen.java
@@ -0,0 +1,53 @@
+package edu.virginia.cs.sgd;
+
+import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.InputAdapter;
+
+public class MenuScreen extends AbstractScreen {
+
+	private static final float BUTTON_WIDTH = 300f;
+	private static final float BUTTON_HEIGHT = 60f;
+	private static final float BUTTON_SPACING = 10f;
+
+	@Override
+	public void resize(int width, int height) {
+		final float buttonX = (width - BUTTON_WIDTH) / 2;
+		float currentY = 280f;
+
+		
+	}
+
+	@Override
+	public void show() {
+
+	}
+
+	@Override
+	public void hide() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void pause() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void resume() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void dispose() {
+		// TODO Auto-generated method stub
+
+	}
+
+}

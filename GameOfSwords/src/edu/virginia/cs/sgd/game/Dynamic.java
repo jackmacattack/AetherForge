886d8cb6a76624a31863f78305ed8be4798c646f
Mitchell Smith <zephyrthenoble@gmail.com>
2013-09-25 21:34:42 -0400

    Added a Dynamic class, for destructible entities

--- GameOfSwords/src/edu/virginia/cs/sgd/game/Dynamic.java
+++ GameOfSwords/src/edu/virginia/cs/sgd/game/Dynamic.java
@@ -1,5 +1,30 @@
 package edu.virginia.cs.sgd.game;
 
 public class Dynamic extends Entity {
+	
+	int health;
+	
+	boolean dead = false;
+	
+	public boolean kill()
+	{
+		if (dead)
+			return false;
+		dead = true;
+		return true;
+	}
+	public boolean resurrect()
+	{
+		if(!dead)
+			return false;
+		dead = false;
+		return true;	
+	}
+	
+	public boolean isDead()
+	{
+		return dead;
+	}
+	
 
 }

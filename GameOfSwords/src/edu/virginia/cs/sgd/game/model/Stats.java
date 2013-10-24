17587a08895bd5c8586de42eaacfcf07a1edb1c4
Mitchell Smith <zephyrthenoble@gmail.com>
2013-10-23 21:15:45 -0400

    added initial values for Stats

--- GameOfSwords/src/edu/virginia/cs/sgd/game/model/Stats.java
+++ GameOfSwords/src/edu/virginia/cs/sgd/game/model/Stats.java
@@ -1,13 +1,24 @@
 package edu.virginia.cs.sgd.game.model;
-
+import com.badlogic.gdx.math.MathUtils;
 public class Stats extends HP {
 
 	private int resolve, defense, strength, intelligence, resistance, dexterity, accuracy;
 	public int experience, next, level, carrying_capacity;
+	public boolean hasTakenTurn = false;
 	
 	public Stats()
 	{
-		
+		resolve = MathUtils.random(3,10);
+		defense = MathUtils.random(3,10);
+		strength = MathUtils.random(3,10);
+		intelligence = MathUtils.random(3,10);
+		resistance = MathUtils.random(3,10);
+		accuracy = MathUtils.random(3,10);
+		dexterity  = MathUtils.random(3,10);
+		carrying_capacity = MathUtils.random(0,2);
+		level = 1;
+		experience = 0;
+		next = 40;
 	}
 
 	public int getResolve() {

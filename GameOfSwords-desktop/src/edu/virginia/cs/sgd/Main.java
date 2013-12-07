4b5fb98fe337805b6bbf7e4238f88e5d6c1f45e1
James <jqw3ha@virginia.edu>
2013-12-07 13:55:49 -0500

    mapscreen goes to credits, changed name in Main

--- GameOfSwords-desktop/src/edu/virginia/cs/sgd/Main.java
+++ GameOfSwords-desktop/src/edu/virginia/cs/sgd/Main.java
@@ -6,7 +6,7 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 public class Main {
 	public static void main(String[] args) {
 		LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
-		cfg.title = "GameOfSwords";
+		cfg.title = "AetherForge";
 		cfg.useGL20 = false;
 		cfg.width = 480;
 		cfg.height = 320;
--- GameOfSwords/src/edu/virginia/cs/sgd/menu/MapScreen.java
+++ GameOfSwords/src/edu/virginia/cs/sgd/menu/MapScreen.java
@@ -48,7 +48,7 @@ public class MapScreen extends AbstractScreen {
 		m.setMusic(music);
 		m.getMusic().play();
 		m.getMusic().setLooping(true);
-		m.setScreen(new SplashScreen(m));
+		m.setScreen(new CreditsScreen(m));
 		this.dispose();
 	}
 
